4dffb9b86909ec7ac61fed16a2947e5bebe8e581
daniel.zat <daniel.zat@cwi.com.br>
2022-02-25 12:25:15 -0300

    ajustes documentação open api

--- src/main/java/com/boasaude/autenticacao/AutenticacaoApplication.java
+++ src/main/java/com/boasaude/autenticacao/AutenticacaoApplication.java
@@ -1,9 +1,12 @@
 package com.boasaude.autenticacao;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Boa Saúde - Autenticação", version = "1.0", description = "Documentação do microserviço de autenticação"))
 public class AutenticacaoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/boasaude/autenticacao/controller/AutenticacaoController.java
+++ src/main/java/com/boasaude/autenticacao/controller/AutenticacaoController.java
@@ -1,11 +1,13 @@
 package com.boasaude.autenticacao.controller;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import com.boasaude.autenticacao.request.LoginRequest;
 import com.boasaude.autenticacao.response.LoginResponse;
@@ -13,14 +15,20 @@ import com.boasaude.autenticacao.service.AutenticacaoService;
 
 import reactor.core.publisher.Mono;
 
-@RequestMapping("/acesso")
+@RequestMapping("/autenticacao")
 @RequiredArgsConstructor
 @RestController
 public class AutenticacaoController {
 
     private final AutenticacaoService autenticacaoService;
 
-    @PostMapping()
+    @PostMapping("/acesso")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Autenticado realizada com sucesso", content = @Content(schema = @Schema(
+                    implementation = LoginResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Autenticação não realizada para este usuário")
+    })
+    @ResponseStatus(HttpStatus.CREATED)
     public Mono<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
 
         return autenticacaoService.verificarLogin(loginRequest);
