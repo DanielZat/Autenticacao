0d688239c37516451cb54b02af56be2de6f7e8bb
daniel.zat <daniel.zat@cwi.com.br>
2022-02-26 23:52:05 -0300

    alteracao de route

--- src/main/java/com/boasaude/autenticacao/controller/AutenticacaoController.java
+++ src/main/java/com/boasaude/autenticacao/controller/AutenticacaoController.java
@@ -16,7 +16,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
-@RequestMapping("/autenticacao")
+@RequestMapping("/acesso")
 @RequiredArgsConstructor
 @RestController
 public class AutenticacaoController {
