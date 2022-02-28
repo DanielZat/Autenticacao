18c4be24dca0b51f33297b259701bdf53be1b8da
daniel.zat <daniel.zat@cwi.com.br>
2022-02-27 22:11:53 -0300

    inclusao de controller para teste

---
+++ src/main/java/com/boasaude/autenticacao/controller/TesteController.java
@@ -0,0 +1,18 @@
+package com.boasaude.autenticacao.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RequestMapping("/teste")
+@RequiredArgsConstructor
+@RestController
+public class TesteController {
+
+    @GetMapping()
+    public Mono<String> teste() {
+        return Mono.just("Chegou aqui");
+    }
+}
